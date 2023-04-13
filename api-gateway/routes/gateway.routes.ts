bd65963873e7d81eb89b28d386d9a598e1f959e5
Abdullah Alqahtani <aalqahtani@manafa.co>
2023-04-13 06:51:04 +0300

    working on microservice1 & api-gateway

--- api-gateway/routes/gateway.routes.ts
+++ api-gateway/routes/gateway.routes.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from "express";
 const router = Router();
 
+const MICROSERVICE1_API_ENDPOINT = process.env.MICROSERVICE1_API_ENDPOINT;
+
 router.get("/microservice1", async (req: Request, res: Response) => {
-  const response = await fetch("http://localhost:5001");
+  const response = await fetch(MICROSERVICE1_API_ENDPOINT + "/api/v1/products"); // microservice1
   const data = await response.json();
   res.json({
     data: data,
